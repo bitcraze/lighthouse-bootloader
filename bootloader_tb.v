884cbf7799b1e1355f8112ad9e32f72b37b11599
Arnaud Taffanel <arnaud@bitcraze.se>
2019-02-17 22:47:40 +0100

    Add I2C transaction to bootloader testbench

--- bootloader_tb.v
+++ bootloader_tb.v
@@ -4,6 +4,10 @@ module testbench;
   // reg clk = 0;
   // initial #10 forever #5 clk = ~clk;
 
+  localparam I2C_ADDRESS = 7'h2f;
+  localparam I2C_READ = 1'b1;
+  localparam I2C_WRITE = 1'b0;
+
   reg clk;
   always #5 clk = (clk === 1'b0);
 
@@ -11,6 +15,10 @@ module testbench;
 
   wire sda;
   assign (pull1,highz0)sda = 1;
+  reg sda_drive = 1;
+  assign (highz1, strong0)sda = sda_drive;
+
+  reg scl = 1;
 
   top #(
     .CLK_FREQ(12000000),
@@ -19,7 +27,8 @@ module testbench;
     .clk(clk),
     .uart0_rx(RX),
     .spi_si(1'b0),
-    .i2c_sda(sda)
+    .i2c_sda(sda),
+    .i2c_scl(scl)
   );
 
   task send_byte;
@@ -39,6 +48,75 @@ module testbench;
     end
   endtask
 
+  task i2c_start;
+    begin
+      sda_drive = 0;
+      repeat (PERIOD) @(posedge clk);
+
+      scl = 0;
+      repeat (PERIOD) @(posedge clk);
+    end
+  endtask
+
+  task i2c_stop;
+    begin
+      sda_drive = 0;
+      repeat (PERIOD) @(posedge clk);
+
+      scl = 1;
+      repeat (PERIOD) @(posedge clk);
+
+      sda_drive = 1;
+      repeat (PERIOD) @(posedge clk);
+    end
+  endtask
+
+  task i2c_write;
+    input [7:0] data;
+    integer i;
+    begin
+      for (i = 0; i < 8; i = i + 1) begin
+        sda_drive = data[7-i];
+        repeat (PERIOD) @(posedge clk);
+        scl = 1;
+        repeat (PERIOD) @(posedge clk);
+        repeat (PERIOD) @(posedge clk);
+        scl = 0;
+        repeat (PERIOD) @(posedge clk);
+      end
+      sda_drive = 1;
+      repeat (PERIOD) @(posedge clk);
+      scl = 1;
+      repeat (PERIOD) @(posedge clk);
+      repeat (PERIOD) @(posedge clk);
+      scl = 0;
+      repeat (PERIOD) @(posedge clk);
+    end
+  endtask
+
+  task i2c_read;
+    integer i;
+    begin
+      for (i = 0; i < 8; i = i + 1) begin
+        sda_drive = 1;
+        repeat (PERIOD) @(posedge clk);
+        scl = 1;
+        repeat (PERIOD) @(posedge clk);
+        repeat (PERIOD) @(posedge clk);
+        scl = 0;
+        repeat (PERIOD) @(posedge clk);
+      end
+      sda_drive = 0;
+      repeat (PERIOD) @(posedge clk);
+      scl = 1;
+      repeat (PERIOD) @(posedge clk);
+      repeat (PERIOD) @(posedge clk);
+      scl = 0;
+      repeat (PERIOD) @(posedge clk);
+    end
+  endtask
+
+
   reg [4095:0] vcdfile;
 
   initial begin
@@ -46,6 +124,33 @@ module testbench;
       $dumpfile(vcdfile);
       $dumpvars(0, testbench);
     end
+
+    repeat (10 * PERIOD) @(posedge clk);
+
+
+    // i2c  test (read flash ID)
+    i2c_start();
+    i2c_write({I2C_ADDRESS, I2C_WRITE});
+    
+    i2c_write(8'h01);
+    i2c_write(8'h02);
+    i2c_write(8'h00);
+    i2c_write(8'h05);
+    i2c_write(8'h00);
+    i2c_write(8'h9F);
+    i2c_write(8'h00);
+    i2c_stop();
+
+    i2c_start();
+    i2c_write({I2C_ADDRESS, I2C_READ});
+
+    i2c_read();
+    i2c_read();
+    i2c_read();
+    i2c_read();
+    i2c_read();
+    i2c_stop();
+
 
     repeat (10 * PERIOD) @(posedge clk);
 
@@ -118,7 +223,7 @@ module testbench;
     repeat (100 * PERIOD) @(posedge clk);
 
     // Boot!
-    send_byte(8'h00);
+    //send_byte(8'h00);
 
     repeat (10 * PERIOD) @(posedge clk);
 
